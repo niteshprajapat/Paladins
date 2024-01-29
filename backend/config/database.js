b310fa352c0cfcf053bc2f264e4f9d42a6b93d21
Nitesh Prajapat <niteshp282000@gmail.com>
2024-01-29 15:15:29 +0530

    setup database functionality using mongodb

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log('MongoDb is connected successfully.');
+    } catch (error) {
+        console.log('MongoDb is not connected.');
+        console.log(error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
