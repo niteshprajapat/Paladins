23c9ede812c0d2b8931df0cef2a8e5944cdffff7
Nitesh Prajapat <niteshp282000@gmail.com>
2024-01-29 15:14:52 +0530

    working on Register API

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,37 @@
+import User from "../models/userModel.js";
+import bcrypt from 'bcryptjs';
+
 
 
 export const register = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
 
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(404).json({
+                success: false,
+                message: "User already Exist.",
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        user = await User.create({
+            username,
+            email,
+            password: hashedPassword,
+        });
+
+        return res.status(200).json({
+            success: true,
+            message: "User Registered successfully.",
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error,
+        })
+    }
 }
