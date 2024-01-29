bc4013eb3abb8e49e6f1b2a867b4bcbb583aaa48
Nitesh Prajapat <niteshp282000@gmail.com>
2024-01-29 21:12:01 +0530

    complete login functionality controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,6 +3,9 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 
+/* API Controllers */
+
+// Register
 export const register = async (req, res) => {
     try {
         const { username, email, password } = req.body;
@@ -32,7 +35,6 @@ export const register = async (req, res) => {
         }).json({
             success: true,
             message: "User Registered successfully.",
-            user,
         })
 
     } catch (error) {
@@ -42,3 +44,47 @@ export const register = async (req, res) => {
         })
     }
 }
+
+
+// Login 
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "Please register first.",
+            });
+        }
+
+        const isMatched = await bcrypt.compare(password, user.password);
+        if (!isMatched) {
+            return res.status(404).json({
+                success: false,
+                message: 'Invalid Credentials',
+            });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+        return res.status(200).cookie("token", token, {
+            httpOnly: true,
+            expires: new Date(Date.now() + 10 * 60 * 1000),
+        }).json({
+            success: true,
+            message: 'User loggedIn successfully.',
+            user,
+        })
+
+
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: error,
+        })
+    }
+}
+
+
