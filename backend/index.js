d69017e84658214bdc954db6377ebfe7c824b7bb
Nitesh Prajapat <niteshp282000@gmail.com>
2024-01-29 14:21:59 +0530

    properly setted up configuration

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,25 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/database.js';
+dotenv.config();
+
 
 const app = express();
 const port = 5000;
 
+
+
+// DB connection
+connectDB();
+
+
+// Middlewares
+app.u
+
+
+
+
+// Home Page API
 app.get("/", (req, res) => {
     res.send({
         success: true,
@@ -10,6 +27,9 @@ app.get("/", (req, res) => {
     });
 })
 
+
+
+// App Listen
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
