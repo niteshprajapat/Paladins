95713351a4fbeff971b7b90652d0e9bf80d43aa6
Nitesh Prajapat <niteshp282000@gmail.com>
2024-01-30 21:40:05 +0530

    React-toastify settup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Header from './components/Header';
 
 const App = () => {
     return (
-        <div>App</div>
+        <Router>
+            <Header />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/register' element={<Register />} />
+                <Route path='/login' element={<Login />} />
+            </Routes>
+        </Router>
     )
 }
 
-export default App
+export default App;
