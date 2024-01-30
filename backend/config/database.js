873edb3ad049df67fefd17832104b51d6de88838
Nitesh Prajapat <niteshp282000@gmail.com>
2024-01-30 21:41:19 +0530

    db names added

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URL);
+        await mongoose.connect(process.env.MONGODB_URL, {
+            dbName: 'Paladins',
+        });
         console.log('MongoDb is connected successfully.');
     } catch (error) {
         console.log('MongoDb is not connected.');
