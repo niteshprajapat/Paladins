95b8b6b1b4e8290306cdc313e397f9a12d811dd3
Nitesh Prajapat <niteshp282000@gmail.com>
2024-02-03 00:07:14 +0530

    working on profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 
 const Profile = () => {
     const { currentUser } = useSelector((store) => store.user);
+    const fileRef = useRef(null);
+    const [image, setImage] = useState(undefined);
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     return (
         <div className='w-full h-screen bg-black/[0.99] p-20'>
@@ -13,7 +18,13 @@ const Profile = () => {
 
                 <form className='flex flex-col gap-6 mt-5'>
                     <div className='self-center'>
+                        <input
+                            onChange={(e) => setImage(e.target.files[0])}
+                            ref={fileRef} type="file" hidden accept='image/*'
+                        />
+
                         <img
+                            onClick={() => fileRef.current.click()}
                             className='w-[70px] h-[70px] rounded-full object-cover'
                             src={currentUser?.rest?.profilePicture}
                             alt="profilepicture"
@@ -21,26 +32,29 @@ const Profile = () => {
                     </div>
                     <div>
                         <input
-                            defaultValue={currentUser?.rest?.username}
+                            onChange={(e) => setUsername(e.target.value)}
+                            defaultValue={currentUser?.user?.username}
                             type="text"
-                            placeholder='Enter your Username'
+                            placeholder='username'
                             className='w-[95%] py-[7px] px-4 text-sm text-white placeholder:text-white/65 placeholder:text-xs bg-[#111] rounded border border-[#111]  focus:outline-none'
                         />
                     </div>
 
                     <div>
                         <input
-                            defaultValue={currentUser?.rest?.email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            defaultValue={currentUser?.user?.email}
                             type="email"
-                            placeholder='Enter your Email'
+                            placeholder='email'
                             className='w-[95%] py-[7px] px-4 text-sm text-white placeholder:text-white/65 placeholder:text-xs bg-[#111] rounded border border-[#111]  focus:outline-none'
                         />
                     </div>
 
                     <div>
                         <input
+                            onChange={(e) => setPassword(e.target.value)}
                             type="password"
-                            placeholder='Enter your Password'
+                            placeholder='password'
                             className='w-[95%] py-[7px] px-4 text-sm text-white placeholder:text-white/65 placeholder:text-xs bg-[#111] rounded border border-[#111]  focus:outline-none'
                         />
                     </div>
