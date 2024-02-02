324675f2c300106afa62a5fa9cba3e3a5656f1af
Nitesh Prajapat <niteshp282000@gmail.com>
2024-02-02 23:33:20 +0530

    working on profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { useSelector } from 'react-redux';
+
 
 const Profile = () => {
-    return (
-        <div className='bg-black/95 w-full h-screen'>
+    const { currentUser } = useSelector((store) => store.user);
 
+    return (
+        <div className='w-full h-screen bg-black/[0.99] p-20'>
+            <div className='w-[30%] mx-auto mt-8 text-white'>
+
+                <h1 className='text-center text-white text-3xl  mb-5'>User Profile</h1>
+
+                <form className='flex flex-col gap-6 mt-5'>
+                    <div className='self-center'>
+                        <img
+                            className='w-[70px] h-[70px] rounded-full object-cover'
+                            src={currentUser?.rest?.profilePicture}
+                            alt="profilepicture"
+                        />
+                    </div>
+                    <div>
+                        <input
+                            defaultValue={currentUser?.rest?.username}
+                            type="text"
+                            placeholder='Enter your Username'
+                            className='w-[95%] py-[7px] px-4 text-sm text-white placeholder:text-white/65 placeholder:text-xs bg-[#111] rounded border border-[#111]  focus:outline-none'
+                        />
+                    </div>
+
+                    <div>
+                        <input
+                            defaultValue={currentUser?.rest?.email}
+                            type="email"
+                            placeholder='Enter your Email'
+                            className='w-[95%] py-[7px] px-4 text-sm text-white placeholder:text-white/65 placeholder:text-xs bg-[#111] rounded border border-[#111]  focus:outline-none'
+                        />
+                    </div>
+
+                    <div>
+                        <input
+                            type="password"
+                            placeholder='Enter your Password'
+                            className='w-[95%] py-[7px] px-4 text-sm text-white placeholder:text-white/65 placeholder:text-xs bg-[#111] rounded border border-[#111]  focus:outline-none'
+                        />
+                    </div>
+
+                    <div className='mt-5'>
+                        <button className='w-[95%]  border border-gray-900 bg-gradient-to-r from-[#111]  via-[#161616] to-[#111]  text-white/65 hover:text-white/80 py-[7px] px-4 text-[13px]  rounded-md hover:opacity-90'>Update Details</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
 
-export default Profile;
+export default Profile
