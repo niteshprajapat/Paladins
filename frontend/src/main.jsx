9f594ccafc251ff41dfebf522c4b930967f9eb03
Nitesh Prajapat <niteshp282000@gmail.com>
2024-02-11 15:17:51 +0530

    refactored code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,12 @@ import App from './App.jsx'
 import './index.css';
 import { Bounce, Slide, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
+    <Provider store={store}>
 
         <ToastContainer
             position="top-right"
@@ -24,7 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         />
 
         <App />
-    </React.StrictMode>,
+    </Provider>
 )
 
 
