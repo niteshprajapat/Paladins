231fd0f759ce28cc71f5a4104170bd6d8b6aaa7c
Nitesh Prajapat <niteshp282000@gmail.com>
2024-02-16 00:09:53 +0530

    delete user api created

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,6 +39,35 @@ export const updateUser = async (req, res) => {
         })
 
     } catch (error) {
+        return res.status(404).json({
+            success: false,
+            message: error.message,
+        })
+    }
+}
 
+
+// Delete User
+export const deleteUser = async (req, res) => {
+    try {
+        if (req.user.id != req.params.id) {
+            return res.status(404).json({
+                success: false,
+                message: 'You can only delete your own account.',
+            })
+        }
+
+        await User.findByIdAndDelete(req.params.id);
+
+        return res.status(200).json({
+            success: true,
+            message: 'User account has been deleted successfully.'
+        })
+
+    } catch (error) {
+        return res.status(404).json({
+            success: false,
+            message: error.message,
+        })
     }
 }
