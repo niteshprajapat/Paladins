ba8a41490d5bc4a42ff2e9f7cc0e8d70d177b5ac
Nitesh Prajapat <niteshp282000@gmail.com>
2024-02-18 23:18:42 +0530

    deleete user functionality added

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,11 +2,12 @@ import express from 'express';
 const router = express.Router();
 
 import { verifyUser } from '../middlewares/verifyToken.js';
-import { updateUser } from '../controllers/userController.js';
+import { updateUser, deleteUser } from '../controllers/userController.js';
 
 
 // API Routes
 router.put('/update/:id', verifyUser, updateUser);
+router.delete('/delete/:id', verifyUser, deleteUser)
 
 
 
